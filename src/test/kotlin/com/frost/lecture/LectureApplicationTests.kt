f1677c446d9414543e469353266d09c366dcced6
hajune <june@smartix.co.kr>
2025-10-10 12:47:22 +0900

    first commit

---
+++ src/test/kotlin/com/frost/lecture/LectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.frost.lecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
